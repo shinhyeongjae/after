22420b7b9adef21e6a90f50b64adfb8abc1eb7a9
shinhyeongjae <63223373+shinhyeongjae@users.noreply.github.com>
2020-04-08 21:21:55 +0900

    [신형재]
    , 하나 더 추가

--- src/doingon/doingon1.java
+++ src/doingon/doingon1.java
@@ -2,6 +2,6 @@ package doingon;
 
 	public class doingon1 {
 		public static void main(String[] args) {
-			System.out.println("doing on now!!,,");
+			System.out.println("doing on now!!,,,");
 		}
 }
